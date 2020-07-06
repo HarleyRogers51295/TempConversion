ddf2e0ed6e972c550f754a5e667de14e5eefc326
Harley Rogers <harleydev512@gmail.com>
2020-07-06 11:39:31 -0500

    Add project files.

---
+++ StaticTempConsoleUI/Program.cs
@@ -0,0 +1,41 @@
+﻿using StaticExerciseConsoleUI;
+using System;
+
+namespace StaticTempConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Temp Converter!");
+
+
+            string decision;
+            string[] choice = { "1. Fahrenheit to Celsius", "2. Celsius to Fahrenheit", "Exit" };
+
+            do
+            {
+            choice[0] = "1";
+            choice[1] = "2";
+            choice[2] = "3";
+
+            Console.WriteLine("Choose which option youd like to do: 1. Fahrenheit to Celsius OR 2. Celsius to Fahrenheit (3) To Exit.");
+            Console.WriteLine("*Enter the number only*");
+            decision = Console.ReadLine();
+            
+                if (decision == "1")
+                {
+                    Console.WriteLine("Enter your Fahrenheit Temp to convert to Celsius ");
+                    int tempF = int.Parse(Console.ReadLine());
+                    Console.WriteLine(TempConverter.FahrenheitToCelsius(tempF));
+                }
+                else if (decision == "2")
+                {
+                    Console.WriteLine("Enter your Celsius Temp to convert to Fahrenheit ");
+                    int tempC = int.Parse(Console.ReadLine());
+                    Console.WriteLine(TempConverter.CelsiusToFahrenheit(tempC));
+                }
+            } while (decision != "3");
+        }
+    }
+}
---
+++ StaticTempConsoleUI/TempConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExerciseConsoleUI
+{
+    static class TempConverter
+    {
+
+       
+        public static int FahrenheitToCelsius(int fahTemp)
+        {
+            int answer = (fahTemp - 32) * 5 / 9;
+            return answer;
+        }
+        public static int CelsiusToFahrenheit(int celTemp)
+        {
+            int answer = (celTemp * 9) / 5 + 32;
+            return answer;
+        }
+
+            
+    }
+}
